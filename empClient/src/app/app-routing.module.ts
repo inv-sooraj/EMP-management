490dc50cd1819412ceedf1e7d15c48f91cfce66d
inno-stejin <stejin.jacob@innovaturelabs.com>
2022-10-21 14:59:56 +0530

    user detail view

--- empClient/src/app/app-routing.module.ts
+++ empClient/src/app/app-routing.module.ts
@@ -4,6 +4,7 @@ import { JobApplyComponent } from './components/job-management/job-apply/job-app
 import { JobListComponent } from './components/job-management/job-list/job-list.component';
 import { JobRequestListComponent } from './components/job-request-management/job-request-list/job-request-list.component';
 import { TestComponentComponent } from './components/test-component/test-component.component';
+import { UserDetailComponent } from './components/user-management/user-detail/user-detail.component';
 import { UserEditComponent } from './components/user-management/user-edit/user-edit.component';
 import { UserListComponent } from './components/user-management/user-list/user-list.component';
 import { UserProfileComponent } from './components/user-management/user-profile/user-profile.component';
@@ -54,6 +55,12 @@ const routes: Routes = [
   {
     path: 'test',
     component: TestComponentComponent,
+    canActivate: [AuthGuard],
+  },
+
+  {
+    path: 'usrdetail',
+    component: UserDetailComponent,
     canActivate: [AuthGuard],
   },
   {
--- empClient/src/app/app.module.ts
+++ empClient/src/app/app.module.ts
@@ -30,6 +30,7 @@ import { ToastrModule } from 'ngx-toastr';
 
 import { UserVerifyComponent } from './core/components/user-verify/user-verify.component';
 import { InfiniteScrollModule } from 'ngx-infinite-scroll';
+import { UserDetailComponent } from './components/user-management/user-detail/user-detail.component';
 
 @NgModule({
   declarations: [
@@ -50,6 +51,7 @@ import { InfiniteScrollModule } from 'ngx-infinite-scroll';
     TestComponentComponent,
     UserchartComponent,
     UserVerifyComponent,
+    UserDetailComponent,
   ],
 
   imports: [
