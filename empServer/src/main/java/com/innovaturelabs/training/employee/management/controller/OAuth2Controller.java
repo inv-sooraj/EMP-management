903dd12cbc2609e75bcba0d6d360fb219c0a05eb
Snehil S <snehil.s@innovaturelabs.com>
2022-10-31 11:57:53 +0530

    Google Signin V3

--- empServer/src/main/java/com/innovaturelabs/training/employee/management/controller/OAuth2Controller.java
+++ empServer/src/main/java/com/innovaturelabs/training/employee/management/controller/OAuth2Controller.java
@@ -2,9 +2,12 @@
 package com.innovaturelabs.training.employee.management.controller;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.innovaturelabs.training.employee.management.service.OAuth2Service;
@@ -17,9 +20,20 @@ public class OAuth2Controller {
     @Autowired
     private OAuth2Service oAuth2Service;
 
-    @PostMapping("/user")
-    public LoginView verifyUser(@RequestBody String idToken) {
-        return oAuth2Service.verifyUser(idToken);
+    @PostMapping("/login")
+    public LoginView login(@RequestBody String idToken) {
+        return oAuth2Service.login(idToken);
+    }
+
+    @PostMapping("/register/{role}")
+    public LoginView register(@RequestBody String idToken, @PathVariable("role") Byte role) {
+        return oAuth2Service.addUser(idToken, role);
+    }
+
+    @GetMapping(value = "/verify-email")
+    public String verifyEMail(@RequestParam("email") String email) {
+
+        return oAuth2Service.checkEmail(email);
     }
 
 }
